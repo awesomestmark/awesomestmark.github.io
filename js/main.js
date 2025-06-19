4503ee74224aabfaa03c91afaf16442cc1387e84
awesomestmark <36779602+awesomestmark@users.noreply.github.com>
2025-06-19 10:15:35 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -41,15 +41,15 @@ function animateSceneText(container) {
 
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("prev-scene").addEventListener("click", () => {
-    if (currentScene > 1) loadScene(currentScene - 1);
+    if (currentScene > 1) {
+      document.body.classList.add("dark-mode"); // Yin
+      loadScene(currentScene - 1);
+    }
   });
 
   document.getElementById("next-scene").addEventListener("click", () => {
+    document.body.classList.remove("dark-mode"); // Yang
     loadScene(currentScene + 1);
-  });
-
-  document.getElementById("toggle-theme").addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
   });
 
   loadScene(currentScene);
